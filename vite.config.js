834f74b42a8fad56f693841c4bff0e078d77a2ce
scarlettjo159 <scarlettjo159@gmail.com>
2026-02-07 17:28:30 +0800

    fix sth

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/valentine-s-day/'
+  base: '/valentine-s-day/',
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })
