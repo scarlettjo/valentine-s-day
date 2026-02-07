6ff8af59598e3577407188dc2f613fad59b4aca6
scarlettjo159 <scarlettjo159@gmail.com>
2026-02-07 17:10:57 +0800

    ???

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 export default defineConfig({
+  base: '/valentine-s-day/',
   plugins: [vue()],
-  base: '/valentine-s-day/'
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  }
 })
